b953779e3598e7679108a24293e4f40ea4a737d2
Mvcampbell3 <mvcampbell3@gmail.com>
2020-03-02 17:26:30 -0500

    request from browser to server to trello and back

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
   console.log('linked');
+  getMemberInfo()
 })
 
+function getMemberInfo() {
+  fetch('/api/trello')
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+}
